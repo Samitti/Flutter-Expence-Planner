992700afae4fa86645e8cb65b8d3027f1fed345b
Samitti <samuelteweldebrhan@gmail.com>
2021-11-02 12:55:12 +0300

    add chart widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import './widgets/new_transactions.dart';
 import '../models/transaction.dart';
+import './widgets/chart.dart';
 import './widgets/transaction_list.dart';
 
 void main() {
@@ -51,6 +52,16 @@ class _MyHomePageState extends State<MyHomePage> {
     //     id: 't2', title: 'New Shirt', amount: 64.99, date: DateTime.now()),
   ];
 
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
+
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
       title: txTitle,
@@ -93,13 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART'),
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_userTransactions),
           ],
         ),
